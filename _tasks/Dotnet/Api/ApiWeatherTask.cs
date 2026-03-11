e8b20a33c58abef7002199266c7a41b11633687b
MariusRuhrmannSyzygy <153537404+MariusRuhrmannSyzygy@users.noreply.github.com>
2026-03-11 15:43:59 +0100

    refactor(dotnet): Update weather api task

--- _tasks/Dotnet/Api/ApiWeatherTask.cs
+++ _tasks/Dotnet/Api/ApiWeatherTask.cs
@@ -1,5 +1,10 @@
 // Verschaffe dir einen Überblick über das Projekt im aktuellen Verzeichnis. 
-// Wir wollen die bestehende Lösung erweitern und die Wetterdaten von einer externen API abrufen. Wie würdest du vorgehen?
+// -------------------------------------------------------------------------
+// Wir wollen die bestehende Lösung erweitern und die Wetterdaten von einer externen API abrufen und wie gehabt über den bestehenden Controller ans Frontend geben.
+// Schau dir dafür die API von OpenWeatherMap an: https://openweathermap.org/forecast16?collection=current_forecast. Wie würdest du vorgehen?
+
 
 // Get an overview of the project in the current directory.
-// We want to extend the existing solution and retrieve the weather data from an external API. How would you proceed?
+// -------------------------------------------------------------------------
+// We want to extend the existing solution and retrieve weather data from an external API and, as usual, provide it to the frontend via the existing controller.
+// Take a look at the API from OpenWeatherMap: https://openweathermap.org/forecast16?collection=current_forecast. How would you proceed?
